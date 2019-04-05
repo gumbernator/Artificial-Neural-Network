a77f9af1abf7fc7f750bb71efc0a1a2d5ad52e96
gumbernator <44472991+gumbernator@users.noreply.github.com>
2019-04-05 10:27:56 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -6,6 +6,13 @@ int main()
 	// DO NOT ERASE THIS LINE !!!!!!!!
 	srand (time(NULL));
 
+
+	// preparing data (XOR)
+	// input -> output
+	// 0 0 -> 0
+	// 0 1 -> 1
+	// 1 0 -> 1
+	// 1 1 -> 0
 	vector<float> data[4];
 	data[0].push_back(0);
 	data[0].push_back(0);
@@ -29,18 +36,47 @@ int main()
 	target[3].push_back(0);
 
 
-
+	// initializing Neural Net
 	NN net;
-	net.addLayer(2);
-	net.addLayer(3, "sigmoid");
-	net.addLayer(1, "sigmoid");
-	string s;
-	cin>>s;
-	net.setOptimizer(s);
-	net.setLearningRate(0.1);
-	cin>>s;
-	net.setLossFunc(s);
 
+	// adding first layer (input layer) with 2 neurons
+	net.addLayer(2);
+	
+	// adding a layer (first hidden layer) with 3 neurons
+	// activation function of this layer is sigmoid
+	net.addLayer(3, "sigmoid");
+	
+	// adding a layer (which can be another hidden layer if you wanna go deep)
+	// activation if this layer is also 
+	net.addLayer(1, "sigmoid");
+
+	// you can add how many layers you like :)
+
+	/* 
+		setting the optimizer for backpropagation. options:
+		"GD" (simple form)
+		"Momentum" (using Momentum to update the parameters)
+		"AdaGrad" (Decaying the learning rate using sum of squared Historic parameters) 
+
+		in this case we used "AdaGrad"
+	*/ 
+	net.setOptimizer("AdaGrad");
+
+	// setting the learning rate to 0.01
+	net.setLearningRate(0.01);
+	
+	/*
+		setting the loss function for out output. options:
+		"MAE" (Mean Absolute Error)
+		"MSE" (Mean Squared Error)
+	 	
+		in this case "MSE" (Mean Square Error)
+	*/
+	net.setLossFunc("MSE");
+
+
+	// let's see how well our network is going to perform
+	// it's gonna be so bad :P
 	for (int i=0; i<4; i++) {
         	   cout<<data[i][0]<<"  "<<data[i][1]<<endl;
         	   net.inputFloat(data[i]);
@@ -48,37 +84,40 @@ int main()
         	   net.printOutput();
 	}
 
-	// vector<Matrix> temp;
-	// Matrix t;
-	// t.setDimensions(2,2);
-	// temp.push_back(t);
-	// t.setDimensions(3,3);
-	// temp.push_back(t);
-
-	// temp[0].print();
-	// temp[1].print();
-
 	float error = 99999999;
 	int counter = 0;
-	float lr = 0.1;
+
+	// now we train!!!
     while (error > 0.005) {
         int select = (int)random(0, 4);
+		// training on a random samples of our xor dataset 
         net.train(data[select], target[select]);
-            
+
+		// calculating the mean square error of the output
 		float sum = 0;
 		for (int i=0; i<4; i++)
 			sum += net.meanSqrError(data[i], target[i]);
 		error = sum / 4.0;
+		
+		// printing the error
 		cout<<"error: "<<error*100<<"%"<<"\r";
 
+		/*
+			Every 40 steps we need to reset the AdaGrad's historic sum
+			...preventing our learning rate to become insignificantly small
+			It's called Tuning learning rate
+		*/
 		counter++;
 		if (counter % 40 == 0)
 		{
 			net.resetAdaGrad();
 		}
+
+		// we will train until the error becomes less than 0.5%
     }
 	cout<<"\n";
 
+	// Now we can see it is doing quite well
 	for (int i=0; i<4; i++) {
 	   cout<<data[i][0]<<"  "<<data[i][1]<<endl;
 	   net.inputFloat(data[i]);
